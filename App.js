4ebe6529d2283bd97c56d92054215dfb93884315
levtus <61996958+alexandsr@users.noreply.github.com>
2023-07-24 22:25:31 +0200

    Start work

--- App.js
+++ App.js
@@ -1,11 +1,46 @@
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, Button, Image, useColorScheme, } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import Search from './source/search.jsx';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useFonts } from 'expo-font';
 
+const Stack = createStackNavigator();
 export default function App() {
+  const [isDarkMode, setIsDarkMode] = useState(false);
+  const colorScheme = useColorScheme();
+  const toggleDarkMode = () => {
+    setIsDarkMode(!isDarkMode);
+  };
+
+  const backgroundColor = isDarkMode ? '#081013' : '#DBE5E9';
+  const textColor = isDarkMode ? '#DBE5E9' : '#081013';
+  const [loaded] = useFonts({
+    Ogreave: require('./assets/fonts/OgreaveBold-d5gl.ttf'),
+  });
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+
+      {/* Control color & style of top bar. */}    
+      <StatusBar backgroundColor={'#081013'} style={'light'}  />
+    
+      <Image
+        source={require('./assets/cap_dark.png')}
+        style={styles.image}
+      />
+              <Text style={styles.header}>scholarship{"\n"}gallery</Text>  
+
+      <Button 
+        title="Go"  
+        onPress={() => console.log('button presed')}
+      />
+     <NavigationContainer>
+      <Stack.Navigator>
+      <Stack.Screen name="Search" component={Search} />
+      </Stack.Navigator>
+    </NavigationContainer>
     </View>
   );
 }
@@ -13,8 +48,29 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    justifyContent: 'center',  
     alignItems: 'center',
-    justifyContent: 'center',
+    marginTop: 0,
+    backgroundColor: '#081013'
   },
+  header: {
+    fontSize: 40,
+    textAlign: 'center',
+    position: 'absolute',
+    top: 80,
+    fontFamily: 'Ogreave',
+  },
+  subtitle: {
+    textAlign: 'center', 
+    color: '#535353',
+    marginBottom: 20
+  },
+  image: {
+    width: 200,
+    height: 200,
+    top: 50,
+    position: 'absolute',
+
+  }
+  
 });
---
+++ acknowledgements.md
@@ -0,0 +1,4 @@
+
+H. H. Agallas font by [deFharo](https://defharo.com/) licensed as Freeware from [https://www.fontspace.com/hh-agallas-font-f18553](https://www.fontspace.com/hh-agallas-font-f18553)
+
+Graduation Cap Logo adapted from [risk-engineering.com](https://risk-engineering.org/related-courses/) under [the CC BY-SA 4.0 License](https://creativecommons.org/licenses/by-sa/4.0/)
